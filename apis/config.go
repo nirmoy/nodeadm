644f48c6806443e89f302c1dccb40f7c5aafe98a
Daniel Lipovetsky <daniel@platform9.com>
2018-12-20 15:12:35 -0800

    Remove KubeProxy and Kubelet top-level fields
    - kube-proxy configuration is an object in kubeadm MasterConfiguration.
    - kubelet configuration is an object in kubeadm MasterConfiguration, and an
      extra-args map in kubeadm NodeConfiguration.

--- apis/config.go
+++ apis/config.go
@@ -2,18 +2,15 @@ package apis
 
 import (
 	kubeadmv1alpha2 "k8s.io/kubernetes/cmd/kubeadm/app/apis/kubeadm/v1alpha2"
-	kubeletconfigv1beta1 "k8s.io/kubernetes/pkg/kubelet/apis/kubeletconfig/v1beta1"
-	kubeproxyconfigv1alpha1 "k8s.io/kubernetes/pkg/proxy/apis/kubeproxyconfig/v1alpha1"
 )
 
 // InitConfiguration specifies the configuration used by the init command
 type InitConfiguration struct {
-	VIPConfiguration    VIPConfiguration                                `json:"vipConfiguration"`
-	MasterConfiguration kubeadmv1alpha2.MasterConfiguration             `json:"masterConfiguration"`
-	KubeProxy           *kubeproxyconfigv1alpha1.KubeProxyConfiguration `json:"kubeProxy"`
-	Kubelet             *kubeletconfigv1beta1.KubeletConfiguration      `json:"kubelet"`
-	NetworkBackend      map[string]string                               `json:"networkBackend"`
-	KeepAlived          map[string]string                               `json:"keepAlived"`
+	MasterConfiguration kubeadmv1alpha2.MasterConfiguration `json:"masterConfiguration"`
+
+	VIPConfiguration VIPConfiguration  `json:"vipConfiguration"`
+	NetworkBackend   map[string]string `json:"networkBackend"`
+	KeepAlived       map[string]string `json:"keepAlived"`
 }
 
 // JoinConfiguration specifies the configuration used by the join command
